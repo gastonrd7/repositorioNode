70d2f247f796e1988872f51940ac1389c6cde777
gastonrd7 <gaston.rd7@gmail.com>
2017-12-20 11:37:22 -0300

    subida miercoles desde casa

--- api-rest/index.js
+++ api-rest/index.js
@@ -11,6 +11,28 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/api/product', (req, res) => {
+    res.status(200).send({products: [] })
+})
+
+app.get('/api/product/:productId', (req, res) => {
+
+})
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: 'El producto se ah recibido' })
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
+})
+
+
 app.listen(port, () => {
     console.log(`Api rest corriendo en localhost:${port}`)
 })
